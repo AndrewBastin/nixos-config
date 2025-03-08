8065cca0e5a5faf08f2e36958db81f4d6f26c757
Andrew Bastin <andrewbastin.k@gmail.com>
2025-03-09 03:19:12 +0530

    feat: add tig to user packages

--- home.nix
+++ home.nix
@@ -37,6 +37,7 @@
     nix-output-monitor
     nautilus
     gnome-keyring
+    tig
 
     # Flameshot compiled with Wayland support (USE_WAYLAND_GRIM flag)
     (flameshot.override { enableWlrSupport = true; })
