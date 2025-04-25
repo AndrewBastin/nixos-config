277a7854e3a9012c17bc995c6b54ddecf80208df
Andrew Bastin <andrewbastin.k@gmail.com>
2025-04-26 02:38:37 +0530

    refactor: move bootloader configuration out of configuration.nix

--- configuration.nix
+++ configuration.nix
@@ -13,9 +13,6 @@
     extra-trusted-public-keys = nixpkgs-python.cachix.org-1:hxjI7pFxTyuTHn2NkvWCrAUcNZLNS3ZAvfYNuYifcEU= devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw=
   '';
 
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
-
   networking.hostName = hostname;
 
   # Enable networking
--- machines/violet/hardware-configuration.nix
+++ machines/violet/hardware-configuration.nix
@@ -8,6 +8,9 @@
     [ (modulesPath + "/installer/scan/not-detected.nix")
     ];
 
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
   boot.initrd.availableKernelModules = [ "nvme" "xhci_pci" "thunderbolt" "usbhid" "usb_storage" "sd_mod" "rtsx_pci_sdmmc" ];
   boot.initrd.kernelModules = [ ];
   boot.kernelModules = [ "kvm-amd" ];
