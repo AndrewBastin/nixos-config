ff1bf0909e7a3dc4816adaea1c151c158a01754f
Andrew Bastin <andrewbastin.k@gmail.com>
2025-06-12 03:09:38 +0530

    fix(uwu): zen not opening up when using the aerospace shortcut when the app is not running

--- machines/uwu/hm-modules/aerospace/scripts/aerospace-open-zen.sh
+++ machines/uwu/hm-modules/aerospace/scripts/aerospace-open-zen.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 # Open Zen Browser and ensure it appears on the current workspace
 
-# Use AppleScript to create a new Zen window via menu
-osascript -e 'tell application "System Events"
-    tell process "Zen"
-        click menu item "New Window" of menu "File" of menu bar 1
-    end tell
-end tell'
+# Check if Zen is running using AppleScript
+if osascript -e 'tell application "System Events" to return exists process "zen"' | grep -q "true"; then
+    # Zen is running, create new window via menu click
+    osascript -e 'tell application "System Events"
+        tell process "zen"
+            click menu item "New Window" of menu "File" of menu bar 1
+        end tell
+    end tell'
+else
+    # Zen is not running, launch it
+    open -a "Zen"
+fi
