e4ac7e2293d3e40ab8a2cac079e5686ad57a8e97
Andrew Bastin <andrewbastin.k@gmail.com>
2025-11-27 09:06:13 +0530

    feat(fern): window rules for home office setup

--- machines/default.nix
+++ machines/default.nix
@@ -94,6 +94,12 @@
         monitorRules = [
           # Built in monitor - Default configs with a 1.6 scale
           "eDP-1, preferred, auto, 1.6"
+
+          # Dell Monitor on office desk
+          "desc:Dell Inc. DELL U2724DE 3SBQ6P3, 2560x1440@120, auto, 1"
+
+          # Lenovo Monitor on office desk
+          "desc:Lenovo Group Limited L24i-30 UPB4NZH2, 1920x1080@75, auto, 1"
         ];
         
         use-unstable-hyprland = true;
