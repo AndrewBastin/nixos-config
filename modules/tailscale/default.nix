1a4e333ee4b44ff9b7f974d8a928160d6f89896b
Andrew Bastin <andrewbastin.k@gmail.com>
2026-02-14 20:07:15 -0800

    refactor(tailscale): remove openssh setup to favor tailscale ssh

--- modules/tailscale/default.nix
+++ modules/tailscale/default.nix
@@ -1,38 +1,18 @@
 # Tailscale Module
 # Enables Tailscale VPN and trusts the tailscale0 interface in the firewall.
 {
-  options = { lib, ... }: {
-    tailscale.enableSSH = lib.mkOption {
-      type = lib.types.bool;
-      default = true;
-      description = "Enable SSH server accessible via Tailscale";
-    };
+  options = { ... }: {
   };
 
-  nixos = { universalConfig ? {}, lib, ... }: {
+  nixos = { ... }: {
     services.tailscale.enable = true;
-
-    # Enable SSH server if configured
-    services.openssh = lib.mkIf universalConfig.tailscale.enableSSH {
-      enable = true;
-      settings = {
-        # NOTE: This is super dumb!!!!!!!! Switch to SSH keys when secret management is setup!
-        # For now, the firewall only trusts tailscale completely, but this is still not super good!
-        PasswordAuthentication = true;
-        KbdInteractiveAuthentication = true;
-      };
-    };
-
-    networking.firewall = {
-      # Trust Tailscale interface completely (allows SSH and all other traffic)
-      trustedInterfaces = [ "tailscale0" ];
-
-      # Optionally: If you want SSH accessible from other interfaces too, uncomment:
-      # allowedTCPPorts = [ 22 ];
-    };
 
     # Don't block boot for waiting on networking
     systemd.network.wait-online.enable = false;
     boot.initrd.systemd.network.wait-online.enable = false;
+  };
+
+  darwin = { ... }: {
+    services.tailscale.enable = true;
   };
 }
