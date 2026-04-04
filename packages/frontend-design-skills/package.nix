db30a1203071f6f2dab016d5e28b9a34f92f50a5
Andrew Bastin <andrewbastin.k@gmail.com>
2026-04-04 08:31:34 +0530

    chore: bump updates

--- packages/frontend-design-skills/package.nix
+++ packages/frontend-design-skills/package.nix
@@ -7,13 +7,13 @@
 
 stdenvNoCC.mkDerivation {
   pname = "frontend-design-skills";
-  version = "2.1.91-unstable-2026-04-02";
+  version = "2.1.92-unstable-2026-04-04";
 
   src = fetchFromGitHub {
     owner = "anthropics";
     repo = "claude-code";
-    rev = "1e03cc7fc40d9bab33f24855a8b5d31ba66205cb";
-    hash = "sha256-0a0UCD0WeUyN0vBaSYXxCaIIv3iT7coduQKpjcQaeQQ=";
+    rev = "b543a256248ce5ff98804b8dfef4cd6247423d98";
+    hash = "sha256-DNRiSbJR77PjLIMK7BC/LN9A1g7q9NPybnPLxngvh9o=";
   };
 
   dontBuild = true;
