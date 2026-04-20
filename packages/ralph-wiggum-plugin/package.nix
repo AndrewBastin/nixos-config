ebc83eee288a911e546b5b724e7b680b291476f6
Andrew Bastin <andrewbastin.k@gmail.com>
2026-04-20 15:03:16 +0530

    chore: bump updates

--- packages/ralph-wiggum-plugin/package.nix
+++ packages/ralph-wiggum-plugin/package.nix
@@ -7,13 +7,13 @@
 
 stdenvNoCC.mkDerivation {
   pname = "ralph-wiggum-plugin";
-  version = "2.1.114-unstable-2026-04-18";
+  version = "2.1.116-unstable-2026-04-20";
 
   src = fetchFromGitHub {
     owner = "anthropics";
     repo = "claude-code";
-    rev = "0385848b4e737831fc3b973d9a78d31950a87d9d";
-    hash = "sha256-Sle1bvuZIJba9B4/PD1ZK89XkrSbPERstcY/kvr6Wcc=";
+    rev = "fe53778ed90fd971bf4ec78fa1f65ccf0536352f";
+    hash = "sha256-1BHp/6dmAydomw3a50ZGxHIpLhvLzEjP2/5ZtxbMg6I=";
   };
 
   dontBuild = true;
