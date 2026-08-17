3f3d23eab8b3a4c1ea2a05e9f29e020e64b1f8ed
Andrew Bastin <andrewbastin.k@gmail.com>
2026-08-17 13:15:55 +0530

    chore: bump updates + update zen-browser nixpkgs pin to nixpkgs-unstable

--- flake.nix
+++ flake.nix
@@ -22,7 +22,7 @@
 
     zen-browser = {
       url = "github:0xc000022070/zen-browser-flake";
-      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.nixpkgs.follows = "nixpkgs-unstable";
     };
 
     llm-agents.url = "github:numtide/llm-agents.nix";
