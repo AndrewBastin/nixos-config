c0ea4722d630fe9b2f000b5c267c5941fd9fb4f5
Andrew Bastin <andrewbastin.k@gmail.com>
2026-08-22 17:46:36 +0530

    chore: bump updates

--- packages/yazi-catppuccin-mocha/package.nix
+++ packages/yazi-catppuccin-mocha/package.nix
@@ -7,13 +7,13 @@
 
 stdenvNoCC.mkDerivation {
   pname = "yazi-catppuccin-mocha";
-  version = "0-unstable-2026-08-07";
+  version = "0-unstable-2026-08-22";
 
   src = fetchFromGitHub {
     owner = "yazi-rs";
     repo = "flavors";
-    rev = "be0b21d0873092a63946cc2678dd700aac945902";
-    hash = "sha256-Dy73TfcrcbCXY9lwDszNgAKLiCAHf1KIwC4Q5U6k21E=";
+    rev = "20b47bfd78880c2674899597fd26bc01b21ff48c";
+    hash = "sha256-NGnfrQdsnQITKCZ0oh6DCxeCR2ozJoPAZetsi3ghHAI=";
   };
 
   dontBuild = true;
